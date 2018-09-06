5348024d55899e34d1d012572ca35dea59aba6a5
Gamepat <phyrofreaky@gmail.com>
2018-09-06 21:08:41 +0200

    Rearrangeing functions

--- src/flex.cpp
+++ src/flex.cpp
@@ -8,20 +8,19 @@ int16_t flexValues[10];
 
 
 
+/* Setup for Flex-Sensors */
+void initFlex() {
+  for(int i = 0; i < 10; i++) {
+    pinMode(flex_pins[i], INPUT);
+  }
+}
+
+
 /* Measures the Values for the Flex-Sensors */
 void readFlex() {
 
   for (int i = 0; i < 10; i++) {
     flexValues[i] = analogRead(flex_pins[i]);
     Serial.println(i + 1 + ": " + flexValues[i]);
-  }
-}
-
-
-
-/* Setup for Flex-Sensors */
-void initFlex() {
-  for(int i = 0; i < 10; i++) {
-    pinMode(flex_pins[i], INPUT);
   }
 }
