b964d61a058b7e872f91284ec8c39d4cb94459a2
unknown <jakob.lengi@gmail.com>
2018-09-24 14:29:35 +0200

    Added calibration for flex sensor

--- src/flex.cpp
+++ src/flex.cpp
@@ -21,7 +21,6 @@ void initFlex() {
 
 // Measures the values for the Flex-Sensors
 void readFlex() {
-
   for (int i = 0; i < 10; i++) {
     flexValues[i] = analogRead(flex_pins[i]);
     Serial.println(i + 1 + ": " + flexValues[i]);
@@ -31,7 +30,19 @@ void readFlex() {
 // Calibrates the range of the Flex-Sensors
 void calibrateFlex() {
 
+  // Measures closed hand values
+  Serial.println("Close your hand to start.");
+  delay(8000);
   for(int i = 0; i < 10; i++) {
-    
+    cal_bend_val[i] = analogRead(flex_pins[i]);
   }
+  Serial.println("Measured!");
+
+  // Measures opened hand values
+  Serial.println("Open your hand as straight as possible.");
+  delay(8000);
+  for(int i = 0; i < 10; i++) {
+    cal_straight_val[i] = analogRead(flex_pins[i]);
+  }
+  Serial.println("Finished!");
 }
