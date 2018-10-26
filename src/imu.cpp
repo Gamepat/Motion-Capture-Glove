02e36d7ead9b2f67b97ab8d5e953fd320f58f775
Gamepat <phyrofreaky@gmail.com>
2018-10-26 16:49:20 +0200

    Rearranged calculation and fixed bug

--- src/imu.cpp
+++ src/imu.cpp
@@ -6,16 +6,19 @@
 #include <Wire.h>
 
 
+bool started = false;
 
 axis_int16_t gyro_raw;
 axis_int16_t accel_raw;
 
-axis_int16_t gyro_offset;
-axis_int16_t accel_offset;
+axis_int16_t gyro_offset = {-143, 437, -268};
+axis_int16_t accel_offset = {968, -66, -588};
 
 axis_float_t rates;
 axis_float_t accel;
 
+float tot_vect;
+axis_float_t gyro_angles;
 axis_float_t accel_angles;
 
 axis_float_t angles;
@@ -24,9 +27,11 @@ axis_float_t angles;
 float last_update = 0.0;
 
 
-
+// initialize the IMU and run calibration
 void imuInit() {
 	mpu_init(MPU_ADR_1);
+
+	Serial.println("Started calibration");
 	mpu_calibrateGyro(MPU_ADR_1, &gyro_offset);
 	mpu_calibrateAccel(MPU_ADR_1, &accel_offset);
 }
@@ -56,18 +61,43 @@ void processAccel() {
 }
 
 
-void calculateAngles() {
-	// TODO: Replace this with the Madgwick-Algorithm
+// calculates angles from the gyroscope data
+void calcGyroAngles() {
+	float dt = (float)(micros() - last_update) / 1000000.0;			// calculates the time since last measurement in seconds
 
-	float dt = (float)((micros() - last_update) / 1000000);
+	gyro_angles.x += rates.x * dt;
+	gyro_angles.y += rates.y * dt;
+	gyro_angles.z += rates.z * dt;
+
 	last_update = micros();
 
+	// gyro_angles.x += gyro_angles.y * sin(rates.z * dt * DEG_TO_RAD);
+	// gyro_angles.y -= gyro_angles.x * sin(rates.z * dt * DEG_TO_RAD);
+}
+
+// calculates angles from the accelerometer data
+void calcAccelAngles() {
 	accel_angles.x = atan2(accel.y, accel.z) * RAD_TO_DEG;
 	accel_angles.y = atan2(-1 * accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)) * RAD_TO_DEG;
+}
 
-	angles.x = GYRO_PART * (angles.x + (rates.x * dt)) + ACCEL_PART * accel_angles.x;
-	angles.y = GYRO_PART * (angles.y + (rates.y * dt)) + ACCEL_PART * accel_angles.y;
-	//angles.z = GYRO_PART * (angles.z + (rates.z * dt)) + ACCEL_PART * accel_angles.z;		//FIXME
+
+// combines the two previous calculated angles to get a more stable output
+void calculateAngles() {
+
+	// use the angles calculated from the accelerometer data for startup, then switch to the complementary filter
+	if (started) {
+		angles.x = GYRO_PART * gyro_angles.x + ACCEL_PART * accel_angles.x;
+		angles.y = GYRO_PART * gyro_angles.y + ACCEL_PART * accel_angles.y;
+		// angles.z = GYRO_PART * gyro_angles.z + ACCEL_PART * accel_angles.z;		//FIXME
+	}
+	else {
+		angles.x = accel_angles.x;
+		angles.y = accel_angles.y;
+		angles.z = accel_angles.z;
+
+		started = true;
+	}
 }
 
 
@@ -78,6 +108,8 @@ void readIMU(axis_float_t *rot_angles) {
 	processGyro();
 	processAccel();
 
+	calcGyroAngles();
+	calcAccelAngles();
 	calculateAngles();
 
 	rot_angles->x = angles.x;
