e34037a780b506c1d26b60e541e42e578f559f9a
Gamepat <phyrofreaky@gmail.com>
2018-11-01 13:19:20 +0100

    Added int32 and double structs

--- src/lib/types.h
+++ src/lib/types.h
@@ -10,5 +10,13 @@ typedef struct {
 } axis_int16_t;
 
 typedef struct {
+  int32_t x, y, z;
+} axis_int32_t;
+
+typedef struct {
   float x, y, z;
 } axis_float_t;
+
+typedef struct {
+  double x, y, z;
+} axis_double_t;
