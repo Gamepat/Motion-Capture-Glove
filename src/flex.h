9b9fcf06caa15d51ae5dd7e275398c5c33c907bb
unknown <phyrofreaky@gmail.com>
2018-11-19 15:48:23 +0100

    Made variable for sensor-amount

--- src/flex.h
+++ src/flex.h
@@ -2,10 +2,10 @@
 
 #include <Arduino.h>
 
-
+#define SENS_AMO 10
 
 // Pin Definition (ADC0 - ADC10)
-const uint8_t flex_pins[10] = {
+const uint8_t flex_pins[SENS_AMO] = {
   A0, A1, A2, A3, A4, A5, 4, 6, 8, 12
 };
 
